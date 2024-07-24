4b1380bd48253f5a8129ec9dc12a1fc264b814f5
Obsqura Trainer <Obsqura Trainer@DESKTOP-C43945V>
2024-07-24 07:47:02 +0530

    First Commit

---
+++ My_Maven/src/main/java/abstracteg/AbstractClass.java
@@ -0,0 +1,13 @@
+package abstracteg;
+
+public abstract class AbstractClass {
+
+	//abstract method creation
+	public abstract void print();
+	//non abstract method
+	public void display()
+	{
+	System.out.println("Non-access method");	
+	}
+	
+}
